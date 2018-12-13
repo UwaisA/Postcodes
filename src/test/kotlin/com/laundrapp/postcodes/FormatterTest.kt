5fc5b352447cf2a474ffef5e5d4b4fcc3f886bf3
Uwais Ashraf <uwaisash@gmail.com>
2018-12-13 21:47:36 +0000

    Add more formmatter tests for basic scenarios requiring no string change

--- src/test/kotlin/com/laundrapp/postcodes/FormatterTest.kt
+++ src/test/kotlin/com/laundrapp/postcodes/FormatterTest.kt
@@ -13,13 +13,13 @@ class FormatterTest {
 
     @Test
     fun `Valid partial postcode formats`() {
-        assertEquals("", Formatter(Locale.US).format("12"))
+        assertEquals("12", Formatter(Locale.US).format("12"))
     }
 
     @Test
     fun `Valid full postcode formats`() {
-        assertEquals("", Formatter(Locale.US).format("12345"))
-        assertEquals("", Formatter(Locale.US).format("12345-1234"))
-        assertEquals("", Formatter(Locale.US).format("12345 1234"))
+        assertEquals("12345", Formatter(Locale.US).format("12345"))
+        assertEquals("12345-1234", Formatter(Locale.US).format("12345-1234"))
+        assertEquals("12345 1234", Formatter(Locale.US).format("12345 1234"))
     }
 }
