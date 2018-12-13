a504c8a68dbc95c979835c96289e6ece1eed2bab
Uwais Ashraf <uwaisash@gmail.com>
2018-12-13 21:46:05 +0000

    Add more formmatter tests

--- src/test/kotlin/com/laundrapp/postcodes/FormatterTest.kt
+++ src/test/kotlin/com/laundrapp/postcodes/FormatterTest.kt
@@ -7,7 +7,19 @@ import java.util.*
 class FormatterTest {
 
     @Test
-    fun `Empty string formats successfully`() {
+    fun `Empty string formats`() {
         assertEquals("", Formatter(Locale.UK).format(""))
+    }
+
+    @Test
+    fun `Valid partial postcode formats`() {
+        assertEquals("", Formatter(Locale.US).format("12"))
+    }
+
+    @Test
+    fun `Valid full postcode formats`() {
+        assertEquals("", Formatter(Locale.US).format("12345"))
+        assertEquals("", Formatter(Locale.US).format("12345-1234"))
+        assertEquals("", Formatter(Locale.US).format("12345 1234"))
     }
 }
