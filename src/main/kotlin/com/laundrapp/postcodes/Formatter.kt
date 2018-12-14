e97a4f1d807570f8e1b9705e5ae3b469efed0d70
Uwais Ashraf <uwaisash@gmail.com>
2018-12-14 09:15:30 +0000

    Use binary search to find separator location in Formatter - improve performance

--- src/main/kotlin/com/laundrapp/postcodes/Formatter.kt
+++ src/main/kotlin/com/laundrapp/postcodes/Formatter.kt
@@ -12,24 +12,47 @@ class Formatter(locale: Locale) {
         val postcodeStripped = postcode.replace(nonAlphanumeric, "")
         if (validator.partialValidate(postcodeStripped)) return postcodeStripped
 
-        return formatFrom(postcodeStripped, 1)
-    }
-
-    private fun formatFrom(postcode: String, startingChar: Int = 0): String {
-        if (startingChar > postcode.length) {
-            return postcode
-        }
-
-        if (validator.partialValidate(postcode.take(startingChar))) {
-            return formatFrom(postcode, startingChar + 1)
-        }
-
-        val separatedPostcode = postcode.take(startingChar-1) + separator
-        if (validator.partialValidate(separatedPostcode)) {
-            return formatFrom(separatedPostcode + postcode.drop(startingChar-1), startingChar)
+        var postcodeOutput = postcodeStripped
+        var newSeparatorLoc = findSeparatorLocation(postcodeOutput)
+        while (newSeparatorLoc != -1) {
+            postcodeOutput = postcodeOutput.take(newSeparatorLoc) + separator + postcodeOutput.drop(newSeparatorLoc)
+            if (validator.partialValidate(postcodeOutput)) {
+                return postcodeOutput
+            } else {
+                newSeparatorLoc = findSeparatorLocation(postcodeOutput)
+            }
         }
 
         throw CouldNotFormatException()
+    }
+
+    /**
+     * Assuming there is a need for a separator, find it's location
+     * @return The index of the character after which a separator needs to be added
+     *          or -1 if no separator position could be found
+     */
+    private fun findSeparatorLocation(postcode: String): Int {
+        var start = 0
+        var end = postcode.length - 1
+
+        while (start <= end) {
+            val middle = (start + end) / 2
+
+            val partialValidateMiddle = validator.partialValidate(postcode.take(middle))
+            if (!partialValidateMiddle) {
+                end = middle - 1
+                continue
+            }
+            val partialValidateMiddlePlusOne = validator.partialValidate(postcode.take(middle + 1))
+
+            if (partialValidateMiddlePlusOne) {
+                start = middle + 1
+                continue
+            }
+
+            return middle
+        }
+        return -1
     }
 
     class CouldNotFormatException : RuntimeException()
