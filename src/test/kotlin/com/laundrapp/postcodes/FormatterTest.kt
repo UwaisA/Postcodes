e8fd4094b16d671e466f48a7eff5e265da707e69
Uwais Ashraf <uwaisash@gmail.com>
2018-12-15 22:20:01 +0000

    Additional tests of existing functionality with different Locale

--- src/test/kotlin/com/laundrapp/postcodes/FormatterTest.kt
+++ src/test/kotlin/com/laundrapp/postcodes/FormatterTest.kt
@@ -6,17 +6,22 @@ import java.util.*
 
 class FormatterTest {
     private val formatterLV = Formatter(Locale.forLanguageTag("en-LV"))
+    private val formatterUS = Formatter(Locale.US)
 
     @Test
     fun `'-' added to postcode where needed`() {
         assertEquals("LV-1234", formatterLV.format("LV1234"))
         assertEquals("LV-12", formatterLV.format("LV12"))
+        assertEquals("12345-6", formatterUS.format("123456"))
+        assertEquals("12345-6789", formatterUS.format("123456789"))
     }
 
     @Test
     fun `'-' not inserted when it is final character of a postcode`() {
         assertEquals("LV", formatterLV.format("LV"))
         assertEquals("LV", formatterLV.format("LV-"))
+        assertEquals("12345", formatterUS.format("12345"))
+        assertEquals("12345", formatterUS.format("12345-"))
     }
 
 }
